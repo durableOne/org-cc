cb1b8be5f2147dc1075fa51b11750b6fd659e8b4
Joseph Riad <joseph.samy.albert@gmail.com>
2023-03-20 22:46:23 -0500

    Added README, improved code
    * No extra newlines inserted after heading titles
    * You can now call `org-cc-display-notes` with a prefix argument to
    force the display of notes

--- org-cc.el
+++ org-cc.el
@@ -1,8 +1,10 @@
 ;;; Package: org-cc.el - Context clues for Org projects
+(require 'cl-lib)
 
 (defcustom org-cc-directory (concat org-directory "org-cc") "Directory in which the context clue files will be stored.")
 (defcustom org-cc-days 14 "Number of days since last work to trigger display of context clues.")
 
+;;;###autoload
 (defun org-cc-make-or-get-note-dir (&optional make-dir)
   "Creates or retrieves the ID for heading at point and uses it to create
    a subdirectory in the style of org-attach under org-cc-directory to
@@ -15,10 +17,13 @@
         (make-directory target-dir t))
     target-dir))
 
+;;;###autoload
 (defun org-cc-edit-cc-file ()
   "Opens the current heading's context clues file."
+  (interactive)
   (find-file (concat (org-cc-make-or-get-note-dir t) "/cc.org")))
 
+;;;###autoload
 (defun org-cc-get-logbook-notes ()
   "Retrieve the contents of the LOGBOOK drawer"
   (save-excursion
@@ -33,20 +38,20 @@
              (end (org-element-property :contents-end elt)))
         (buffer-substring-no-properties beg end)))))
 
+;;;###autoload
 (defun org-cc-get-time-since-last-work ()
   "Returns the time since you last clocked in on the heading at point.
    Returns nil if it can't find clocking data."
   (let ((logbook (org-get-logbook-notes)))
     (if logbook
         (save-match-data
-          (string-match "^CLOCK: \\(.*\\)$" logbook)
+          (string-match "^CLOCK: \\(.*--\\[.*\\)$" logbook)
           (let ((clock (match-string 1 logbook))) 
             (if clock
                 (progn
-                  ;; Match either [timestamp] (in case clock is still active)
-                  ;; or -[timestamp] to retrieve the last time you clocked out.
-                  (string-match "\\(-\\|^\\)\\[\\([^\]]+\\)\\]" clock)
-                  (org-time-stamp-to-now (match-string 2 clock)))
+                  ;; Match --[timestamp] to retrieve the last time you clocked out.
+                  (string-match "--\\[\\([^\]]+\\)\\]" clock)
+                  (org-time-stamp-to-now (match-string 1 clock)))
               nil)))
       nil)))
 
@@ -60,6 +65,7 @@
 (defun org-cc--get-heading ()
   (org-no-properties (org-get-heading t t t t)))
 
+;;;###autoload
 (defun org-cc-get-notes-files ()
   "Returns a list of pairs (heading . heading's notes file) for the heading at point and all its parents.
    The list goes from the topmost parent down to the current heading."
@@ -70,31 +76,36 @@
                         (setf result (cons (cons (org-cc--get-heading) one-level-up) result)))))
     result))
 
-(defun org-cc-display-notes ()
+;;;###autoload
+(defun org-cc-display-notes (&optional no-time-check)
   "Retrieve all the notes for this heading and its parents and concat them to a buffer"
-  (let ((last-worked (org-cc-get-time-since-last-work)))
+  (interactive "P")
+  (let ((last-worked (org-cc-get-time-since-last-work))
+        (no-time-check (or no-time-check nil))) 
     (when (or
+           no-time-check
            (not last-worked)
            (<= last-worked (- org-cc-days)))
       (let ((buffer (get-buffer-create "*Org Context Clues*"))
             (headings-and-contents (org-cc-get-notes-files)))
-        (switch-to-buffer-other-window buffer)
-        (kill-region (point-min) (point-max))
-        (org-mode)
-        (cl-labels ((insert-heading-and-contents (heading-and-contents level)
-                      ;; Collect notes for all headings and insert them into the buffer
-                      ;; Recursively cdrs down the list produced by org-cc-get-notes-files
-                      (when (not (null heading-and-contents))
-                        (let ((first-entry (car heading-and-contents)))
-                          (insert (concat (s-repeat level "*")
-                                          " "
-                                          (car first-entry)
-                                          "\n"))
-                          (when (cdr first-entry)
-                            (insert (with-temp-buffer (insert-file-contents (cdr first-entry))
-                                                      (buffer-string))))
-                          (insert "\n"))
-                        (insert-heading-and-contents (cdr heading-and-contents) (1+ level)))))
-          (insert-heading-and-contents headings-and-contents 1))))))
+        (unless (cl-every 'null (mapcar 'cdr headings-and-contents))
+          (switch-to-buffer-other-window buffer)
+          (kill-region (point-min) (point-max))
+          (org-mode)
+          (insert "#+title: Org Context Clues\n\n")
+          (cl-labels ((insert-heading-and-contents (heading-and-contents level)
+                        ;; Collect notes for all headings and insert them into the buffer
+                        ;; Recursively cdrs down the list produced by org-cc-get-notes-files
+                        (when (not (null heading-and-contents))
+                          (let ((first-entry (car heading-and-contents)))
+                            (insert (concat (s-repeat level "*")
+                                            " "
+                                            (car first-entry)
+                                            "\n"))
+                            (when (cdr first-entry)
+                              (insert (with-temp-buffer (insert-file-contents (cdr first-entry))
+                                                        (buffer-string)))))
+                          (insert-heading-and-contents (cdr heading-and-contents) (1+ level)))))
+            (insert-heading-and-contents headings-and-contents 1)))))))
 
 (provide 'org-cc)
